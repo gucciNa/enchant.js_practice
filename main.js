5497e70f286a9ef4f3ad35307eee0438492ffc1e
huncro <fallere417@yahoo.co.jp>
2015-07-17 20:39:51 +0900

    なんとかなったかも

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ window.onload = function() {
  
     core.preload('chara5.png');
     core.preload('chara7.png');
-    core.fps = 14;
+    core.fps = 15;
     
     core.onload = function() {
 
         var knight = new Sprite(32, 32);
         knight.image = core.assets['chara5.png'];
-        knight.x = 10;
-        knight.y = 10;
+        knight.x = 2 * 16 -8; //ここ変えたらできたっ！
+        knight.y = 2 * 16;
         knight.scale(1.3,1.3);
         //knight.scale = 19;
         knight.isMoving = false;
@@ -23,33 +23,22 @@ window.onload = function() {
         var spacePush = 0; //あとで考える!
         var cnt = 0;  //攻撃フレームアニメーション
 		  
-        knight.addEventListener('enterframe', function(e) {
+        knight.addEventListener('enterframe', function(e) {   //もっと細かく動きたい
             this.frame = this.direction * 9 + this.walk;
             if (this.x > 320) this.x = 0;
             
-            /*if(spacePush == 0){
-              this.frame = this.direction * 9 + this.walk; // 攻撃してない時のフレーム設定
-              cnt =0;
-            } else {
-              if(cnt < 4){
-                this.frame = this.direction * 9 + this.walk + 6; // 攻撃用フレーム設定
-                cnt++;
-              } else if(this.isMoving) {
-                 this.frame = this.direction * 9 + this.walk;
-              } else {
-                 this.frame = this.direction * 9;
-              }
-	    }*/
-	    if (this.isMoving) {
-		/*this.moveBy(this.vx, this.vy);
+            
+	    
+            if (this.isMoving) {
+		this.moveBy(this.vx, this.vy);
                 if (!(core.frame % 3)) { 
                    this.walk++; // walkを1増加
                    this.walk %= 3; // walkを3で割った余りに＝0,1,2のどれかにする
                 }
-                if ((this.vx && this.x % 16 == 0) || (this.vy && (this.y-8) % 16 == 0)) { //次マスに移動しきったらとまる
+                if ((this.vx && (this.x-8) % 16 == 0) || (this.vy && this.y % 16 == 0)) { //次マスに移動しきったらとまる
                    this.isMoving = false; // isMovingをFalseに
                    this.walk = 1; // walkを1に
-                }  */              
+                }              
             } else { 
                 this.vx = this.vy = 0;
                 if (core.input.left) {
@@ -65,18 +54,33 @@ window.onload = function() {
                    this.direction = 0;
                    this.vy = 4;
                 }
-                /*if (this.vx || this.vy) { // vxかvyが0じゃない場合＝動こうとしている場合
-                   var x = this.x + (this.vx ? this.vx / Math.abs(this.vx) * 16 : 0) + 8;
-                   var y = this.y + (this.vy ? this.vy / Math.abs(this.vy) * 16 : 0) + 8;
-                   if (0 <= x && x < map.width && 0 <= y && y < map.height && !map.hitTest(x, y)) {
+                if (this.vx || this.vy) { // vxかvyが0じゃない場合＝動こうとしている場合
+                   var x = this.x + (this.vx ? this.vx / Math.abs(this.vx) * 16 : 0) + 16;
+                   var y = this.y + (this.vy ? this.vy / Math.abs(this.vy) * 16 : 0) + 16;
+                   if (0 <= x && x < 320 && 0 <= y && y < 320/* && !map.hitTest(x, y)*/) {
                       this.isMoving = true;
                       arguments.callee.call(this);
                    }
-                }*/
+                }
 		if (!(core.frame % 3) && spacePush) {
 			this.walk++;
                         this.walk %= 3;
                 }
+
+                if(spacePush == 0){  //攻撃もっと連発したい
+                  this.frame = this.direction * 9 + this.walk; // 攻撃してない時のフレーム設定
+                  cnt =0;
+                } else {
+                    if (cnt < 4){
+                       this.frame = this.direction * 9 + this.walk + 6; // 攻撃用フレーム設定
+                       cnt++;
+                    } else if (this.isMoving){
+                              this.frame = this.direction * 9 + this.walk;
+                    } else {
+                       this.frame = this.direction * 9;
+                    }
+                }  
+ 
             }
 
 	    
@@ -95,13 +99,13 @@ window.onload = function() {
 
         core.keybind(32,'space');
 	core.addEventListener("spacebuttondown" , function(e) {
-            spPush = 1;        
-
+            spacePush = 1;        
+            knight.frame = knight.age % 3 + 6;
 
 
         });
 	core.addEventListener("spacebuttonup" , function(e){
-            spPush = 0;
+            spacePush = 0;
         });
 
 
